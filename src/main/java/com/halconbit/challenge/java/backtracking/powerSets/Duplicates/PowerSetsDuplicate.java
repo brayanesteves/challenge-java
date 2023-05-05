87e54efde313152cb73d0caa693308e701727c76
Brayan Esteves <brayan.esteves93@gmail.com>
2023-05-05 16:33:38 -0400

    Backtracking - Power Sets (Duplicates)

--- src/main/java/com/halconbit/challenge/java/backtracking/powerSets/Duplicates/PowerSetsDuplicate.java
+++ src/main/java/com/halconbit/challenge/java/backtracking/powerSets/Duplicates/PowerSetsDuplicate.java
@@ -1,23 +1,28 @@
-package com.halconbit.challenge.java.backtracking.powerSets.Duplicate;
+package com.halconbit.challenge.java.backtracking.powerSets.Duplicates;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Equations:
- * O(n * 2^n), O(n)
+ * TC: O(n * 2^n), SC: O(n)
  * @author Brayan Esteves
  */
-public class PowerSetsUnique {
+public class PowerSetsDuplicate {
     private void dfs(int[] nums, List<List<Integer>> list, List<Integer> sub, int start) {
         list.add(new ArrayList<>(sub));
         for(int i = start; i < nums.length; i++) {
+            if(i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
             sub.add(nums[i]);
             dfs(nums, list, sub, i + 1);
             sub.remove(sub.size() - 1);
         }
     }
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
         dfs(nums, list, new ArrayList<>(), 0);
         return list;
--- src/main/java/com/halconbit/challenge/java/index.java
+++ src/main/java/com/halconbit/challenge/java/index.java
@@ -2,7 +2,8 @@ package com.halconbit.challenge.java;
 
 import com.halconbit.challenge.java.backtracking.arrayPermutation.Duplicate.ArrayDuplicates;
 import com.halconbit.challenge.java.backtracking.arrayPermutation.Unique.Unique;
-import com.halconbit.challenge.java.backtracking.powerSets.Duplicate.PowerSetsUnique;
+import com.halconbit.challenge.java.backtracking.powerSets.Duplicates.PowerSetsDuplicate;
+import com.halconbit.challenge.java.backtracking.powerSets.Unique.PowerSetsUnique;
 import com.halconbit.challenge.java.backtracking.stringPermutation.Duplicates.Duplicates;
 import com.halconbit.challenge.java.backtracking.stringPermutation.NoDuplicates.NoDuplicates;
 import java.util.ArrayList;
@@ -17,16 +18,18 @@ public class index {
     
     public static void main(String[] args) {
         
-        NoDuplicates    noDuplicates    = new NoDuplicates();
-        Duplicates      duplicates      = new Duplicates();
-        Unique          unique          = new Unique();
-        ArrayDuplicates arrayUnique     = new ArrayDuplicates();
-        PowerSetsUnique powerSetsUnique = new PowerSetsUnique();
+        NoDuplicates       noDuplicates       = new NoDuplicates();
+        Duplicates         duplicates         = new Duplicates();
+        Unique             unique             = new Unique();
+        ArrayDuplicates    arrayUnique        = new ArrayDuplicates();
+        PowerSetsUnique    powerSetsUnique    = new PowerSetsUnique();
+        PowerSetsDuplicate powerSetsDuplicate = new PowerSetsDuplicate();
         
         String str                     = "";
         int[] numsA                    = {1, 2, 3};
         int[] numsB                    = {1, 1, 2};
         int[] numsC                    = {1, 2, 3};
+        int[] numsD                    = {1, 2, 2};
         List<String> list              = new ArrayList<>();
         List<List<Integer>> listToList = new ArrayList<>();
         
@@ -69,6 +72,14 @@ public class index {
         System.out.println("POWER SETS - DUPLICATE UNIQUE");
         
         listToList = powerSetsUnique.subsets(numsC);
+        for(int i = 0; i < listToList.size(); i++) {
+            System.out.println(listToList.get(i));
+        }
+        
+        System.out.println("================");        
+        System.out.println("POWER SETS - DUPLICATES");
+        
+        listToList = powerSetsDuplicate.subsetsWithDup(numsD);
         for(int i = 0; i < listToList.size(); i++) {
             System.out.println(listToList.get(i));
         }
