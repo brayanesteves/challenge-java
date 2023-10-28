818190b5cea916bf73af70d2cb07dfef5c40f43b
Brayan Esteves <brayan.esteves93@gmail.com>
2023-10-28 13:51:00 -0400

    OOP - Lambda: Refactor (Functional 'for' & 'forEach') [28/10/2023]

--- src/main/java/com/halconbit/challenge/java/oop/SalesOfProducts/controller/Functional.java
+++ src/main/java/com/halconbit/challenge/java/oop/SalesOfProducts/controller/Functional.java
@@ -6,15 +6,19 @@ import java.util.List;
 public class Functional {
     
     public void withFor(List<Person>  persons) {
+        System.out.println("<FOR>");
         for(int i = 0; i < persons.size(); i++) {
             System.out.println(persons.get(i));
         }
+        System.out.println("</FOR>");
     }
     
     public void withForEach(List<Person>  persons) {
+        System.out.println("<FOR EACH>");
         for(Person person : persons) {
             System.out.println(person);
         }
+        System.out.println("</FOR EACH>");
     }
     
 }
