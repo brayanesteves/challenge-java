99ca9bda259ce749161c8a92a6d77cd752f2a3f0
Brayan Esteves <brayan.esteves93@gmail.com>
2023-11-11 21:49:55 -0400

    Amphitheatre - Assignment OOP [11/11/2023]

---
+++ src/main/java/com/halconbit/challenge/java/oop/Amphitheatre/Amphitheatre.java
@@ -0,0 +1,62 @@
+package com.halconbit.challenge.java.oop.Amphitheatre;
+
+/**
+ * @author Brayan Esteves
+ * Challenge:
+ * [ENGLISH]:
+ * Practice Exercise.
+ * TRAINEE Technical Test.
+ * 2nd Part - OOP.
+ * Based on the same scenario of the seat reservation system for an amphitheater,
+ * the time has come to propose the "ticket system" that each person will obtain
+ * when reserving their seat. For this, consider the following:
+ * 1) Create the "template" necessary to store the following data per ticket,
+ *    number, row, seat, purchase date, validity date, price.
+ * 2) Use a "logical database" through some data structure that allows storing
+ *    an undetermined number of tickets. Load a series of tickets into said structure.
+ * 3) Create a method that goes through the selected structure and sums the prices
+ *    of all tickets. Show the total on screen. GUI is not necessary for this.
+ * 4) Ask the user to enter a row number. Based on this, display on screen the
+ *    data of the tickets belonging to that row. GUI is not necessary for this.
+ * 5) Considerations: Neither GUI nor DB implementation is necessary. 100% logical
+ *    handling of application development will be evaluated.
+ * 6) Extra: In case you want to add a "template" for clients with the following
+ *    data: id, identity document, name, surname. How would it be implemented?
+ *    In what way would it relate to the ticket so that it has the data of the
+ *    client assigned to it? Carry out the implementation.
+ * [=======]
+ * [SPANISH]:
+ * Ejercicio Práctico.
+ * Prueba técnica TRAINEE.
+ * 2da Parte - POO.
+ * Basándose en el mismo escenario del sistema de reserva de asientos para un
+ * anfiteatro, llegó el momento de plantear el "sistema de tickets" que obtendrá
+ * cada persona al reservar su asiento. Para ello, tener en cuenta lo siguiente:
+ * 1) Crear la "plantilla" necesaria para guardar los siguientes datos por ticket,
+ *    número, fila, asiento, fecha de compra, fecha de validez, precio.
+ * 2) Usar una "base de datos lógica" mediante  alguna estructura de datos que
+ *    permita almacenar un número indeterminado de tickets.Cargar una serie de
+ *    tickets a dicha estructura.
+ * 3) Crear un método que recorra la estructura seleccionada y sume los precios
+ *    de todos los tickets. Mostrar por pantalla el total. No es necesario usar
+ *    IGU para esto.
+ * 4) Solicitar el usuario que ingrese un número de fila. A partir de esto, mostrar
+ *    por pantalla los datos de los tickets pertenecientes a esa fila. No es
+ *    necesario usar IGU para esto.
+ * 5) Consideraciones: No es necesaria implementación ni de IGU ni de BD. Se
+ *    evaluará 100% el manejo lógico del desarrollo de la aplicación.
+ * 6) Extra: En caso que se desee agregar una "plantilla" para clientes con los
+ *    datos: id, documento de identidad, nombre, apellido. ¿Cómo se implementaría?
+ *    ¿De qué manera se relacionaría con ticket para que el mismo posea los datos
+ *    del cliente que tiene asignado? Realizar la implementación.
+ */
+public class Amphitheatre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
