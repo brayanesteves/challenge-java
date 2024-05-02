98312795bd19910f0d89cc050b06b6fe88612abc
Brayan Esteves <brayan.esteves93@gmail.com>
2024-05-02 09:00:50 -0400

    Lambda(Java [Version 8]) - Numeric [DoubleStream] using "of(), iterate(), generate(), range() & rangeClosed()" [02/05/2024]

--- src/main/java/com/halconbit/challenge/java/lambda/java/version/eight/lambda/streams/numeric/DoubleStreamExample.java
+++ src/main/java/com/halconbit/challenge/java/lambda/java/version/eight/lambda/streams/numeric/DoubleStreamExample.java
@@ -1,37 +1,38 @@
 package com.halconbit.challenge.java.lambda.java.version.eight.lambda.streams.numeric;
 
 import java.util.Random;
+import java.util.stream.DoubleStream;
 import java.util.stream.LongStream;
 
-public class LongStreamExample {
+public class DoubleStreamExample {
     
     public void using_of() {
         // Using of().
-        LongStream numbers = LongStream.of(1, 2, 3, 4, 5);
+        DoubleStream numbers = DoubleStream.of(1.2, 2.2, 3.3, 4.4, 5.5);
         numbers.forEach(System.out::println);
     }
     
     public void using_iterate(int value) {
         // Using of().
-        LongStream numbers = LongStream.iterate(0, i -> i + 2).limit(value);
+        DoubleStream numbers = DoubleStream.iterate(0, i -> i + 2.0).limit(value);
         numbers.forEach(System.out::println);
     }
     
     public void using_generate(int value) {
         // Using of().
-        LongStream numbers = LongStream.generate(new Random()::nextLong).limit(value);
+        DoubleStream numbers = DoubleStream.generate(new Random()::nextDouble).limit(value);
         numbers.forEach(System.out::println);
     }
     
     public void using_range(long a, long b) {
         // Using of().
-        LongStream numbers = LongStream.range(a, b);
+        DoubleStream numbers = LongStream.range(a, b).asDoubleStream();
         numbers.forEach(System.out::println);
     }
     
     public void using_rangeClosed(long a, long b) {
         // Using of().
-        LongStream numbers = LongStream.rangeClosed(a, b);
+        DoubleStream numbers = LongStream.rangeClosed(a, b).asDoubleStream();
         numbers.forEach(System.out::println);
     }
     
