d7443d91b74b7c8b184cc1a376c819023f666c67
Brayan Esteves <brayan.esteves93@gmail.com>
2024-05-03 15:41:49 -0400

    Lambda(Java [Version 8]) -  java.util.Date and java.sql.Date to LocalDateTime/LocalDate [03/05/2024]

---
+++ src/main/java/com/halconbit/challenge/java/lambda/java/version/eight/newdatetimeapis/ConvertToLocalDateTime.java
@@ -0,0 +1,25 @@
+package com.halconbit.challenge.java.lambda.java.version.eight.newdatetimeapis;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class ConvertToLocalDateTime {
+    
+    private Date          date           = new Date();
+    private LocalDateTime localDateTime;
+    private LocalDate     localDate;
+    private java.sql.Date dateSql;
+    
+    public LocalDateTime atZone() {
+        this.localDateTime = this.date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        return this.localDateTime;
+    }
+    
+    public LocalDate dateSql() {
+        this.dateSql = new java.sql.Date(System.currentTimeMillis());
+        return this.localDateTime;
+    }
+    
+}
