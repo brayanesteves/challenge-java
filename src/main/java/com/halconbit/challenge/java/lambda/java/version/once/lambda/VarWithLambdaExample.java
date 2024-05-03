b65eda51828c4212f3143708c070c92bb7a8ae39
Brayan Esteves <brayan.esteves93@gmail.com>
2024-05-03 17:26:44 -0400

    (Java [Version 11]) - Adding 'var' to Lambda [03/05/2024]

---
+++ src/main/java/com/halconbit/challenge/java/lambda/java/version/once/lambda/VarWithLambdaExample.java
@@ -0,0 +1,22 @@
+package com.halconbit.challenge.java.lambda.java.version.once.lambda;
+
+import com.halconbit.challenge.java.lambda.java.version.eight.lambda.expressions.functionalinterfaces.controller.InstructorController;
+import com.halconbit.challenge.java.lambda.java.version.eight.lambda.expressions.functionalinterfaces.models.Instructor;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
+public class VarWithLambdaExample {
+    
+    public void listInstructors() {
+        var instructors = InstructorController.getAll();
+        Predicate<Instructor> experiencePredicate = (var s) -> s.getYearsOfExperience() > 10;
+        instructors.forEach(instructor -> {
+            if(experiencePredicate.test(instructor)) {
+                var result = instructor.getName();
+            }
+        });
+        
+        BiFunction<Integer, Integer, Integer> sum = (var x, var y) -> x + y;
+    }
+    
+}
